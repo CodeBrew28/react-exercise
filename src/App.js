2ca6fd39fadde875762c89b47d7dcf1ee81dd2a9
arshiam2 <arshiam2@illinois.edu>
2018-10-12 23:35:42 -0500

    update

--- src/App.js
+++ src/App.js
@@ -6,34 +6,34 @@ import './styles/app.css'
 class App extends Component {
   state = {
     initialCount: 4,
-    newCount: "0",
+    newCount: '0',
     items: ['A', 'Boba', 'B', 'Milk', 'Bread', 'C']
-  };
+  }
 
   handleSubmit = () => {
     this.setState({
       initialCount: parseInt(this.state.newCount)
-    });
-  };
+    })
+  }
 
   handleTextChange = event => {
     this.setState({
       newCount: event.target.value
-    });
-  };
+    })
+  }
 
   render() {
     return (
       <div className="app">
         <h1>Hello World!</h1>
-        <input 
+        <input
           type="text"
           value={this.state.newCount}
           onChange={this.handleTextChange}
         />
         <button onClick={this.handleSubmit}>Submit</button>
-        <Counter initialCount={this.state.initialCount}/>
-        <Instructions shouldDisplayImage={true} items={this.state.items}/>
+        <Counter count={this.state.initialCount} />
+        <Instructions shouldDisplayImage={true} items={this.state.items} />
       </div>
     )
   }
